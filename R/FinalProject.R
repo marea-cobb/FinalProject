56578bff8dbfabf72abe7b6d9dc32836ea024957
marea-cobb <marea.cobb@email.wsu.edu>
2015-03-09 17:35:38 -0700

    Pushing changes to the final presentation introduction.

--- R/FinalProject.R
+++ R/FinalProject.R
@@ -1,14 +1,15 @@
-source("http://www.bioconductor.org/biocLite.R")
-biocLite("GEOquery")
+# source("http://www.bioconductor.org/biocLite.R")
+# biocLite("GEOquery")
+# 
+# library(Biobase)
+# library(GEOquery)
+# 
+# ## This will download the entire database, so can be slow
+# if (!file.exists("GEOmetadb.sqlite")) {
+#   # Download database only if it's not done already
+#   getSQLiteFile
+# }
 
-library(Biobase)
-library(GEOquery)
-
-## This will download the entire database, so can be slow
-if (!file.exists("GEOmetadb.sqlite")) {
-  # Download database only if it's not done already
-  getSQLiteFile
-}
-
-if (!file.exists("Data/"))
-gse37 <- getGEO('GSE37', destdir=".")
+help(read.table)
+df = read.csv("BRCA.csv", header = FALSE)
+head(df)
